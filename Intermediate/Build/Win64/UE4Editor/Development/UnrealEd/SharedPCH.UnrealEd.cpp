a2c795c864952bf21a2c4a51106f077ebe8d6884
ItzChrisXx <chrisquindt@googlemail.com>
2018-11-08 19:41:45 +0100

    Gravitation Ability is now steerable and has a directional force

--- Intermediate/Build/Win64/UE4Editor/Development/UnrealEd/SharedPCH.UnrealEd.cpp
+++ Intermediate/Build/Win64/UE4Editor/Development/UnrealEd/SharedPCH.UnrealEd.cpp
@@ -1,1 +1,1 @@
-#include "C:/Users/Chris/Documents/Unreal Projects/Perforce/GravRacer/Intermediate/Build/Win64/UE4Editor/Development/UnrealEd/SharedPCH.UnrealEd.h"
+#include "C:/Users/Chris/Documents/Unreal Projects/GravRacer/Intermediate/Build/Win64/UE4Editor/Development/UnrealEd/SharedPCH.UnrealEd.h"
